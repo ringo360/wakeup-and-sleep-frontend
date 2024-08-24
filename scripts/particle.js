6086acb500cf143e2e93c5fb59f07f78ea16adc3
ringoXD <ringoxd@ringoxd.dev>
2024-08-24 03:17:47 +0000

    overlay

---
+++ scripts/particle.js
@@ -0,0 +1,6 @@
+import { Fireworks } from 'https://cdn.skypack.dev/fireworks-js';
+
+//https://paiza.hatenablog.com/entry/2022/08/17/143000
+const container = document.getElementById('overlay');
+const fireworks = new Fireworks(container);
+// fireworks.start()
